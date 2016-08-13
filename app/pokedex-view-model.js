f9747644cb94230b4f5cd20dd5bd722ee71712f4
Mike Branstein <mike.branstein@gmail.com>
2016-08-13 19:21:01 -0400

    nav updated

--- app/pokedex-view-model.js
+++ app/pokedex-view-model.js
@@ -7,17 +7,17 @@ var PokedexModel = (function (_super) {
     function PokedexModel() {
         _super.call(this);
         this.imageNumber = 0;
+        this.prevImageNumber = 269;
+        this.nextImageNumber = 1;
         this.lowerBound = 0;
 
         var appFolder = fs.knownFolders.currentApp();
         var file = appFolder.getFile("pokedex.json");
 
-        file.readText("Unicode").then(function(content) {
+        file.readText("ASCII").then(function(content) {
             this.pokedex = JSON.parse(content);
         }.bind(this)).then(function () {
-            this.set("imageSource", "~/images/" + this.pokedex[this.imageNumber].fileName);
-            this.set("name", this.pokedex[this.imageNumber].name);
-            this.set("description", this.pokedex[this.imageNumber].description);
+            this.update();
         }.bind(this));
     };
 
@@ -30,28 +30,55 @@ var PokedexModel = (function (_super) {
         return number + ""; // always return a string
     };
 
-    PokedexModel.prototype.changePokemon = function (direction) {
-        console.log(this.imageNumber);
-        tts.speak("");
-        
-        if (direction === -1 && this.imageNumber === this.lowerBound)
-            this.imageNumber = this.pokedex.length - 1;
-        else if (direction === 1 && this.imageNumber === this.pokedex.length - 1) 
-            this.imageNumber = this.lowerBound;
+    PokedexModel.prototype.getImageNumber = function (num, direction) {
+        var result = num;
+
+        if (direction === -1 && num === this.lowerBound)
+            result = this.pokedex.length - 1;
+        else if (direction === 1 && num === this.pokedex.length - 1) 
+            result = this.lowerBound;
         else 
-            this.imageNumber += direction;
+            result += direction;
+
+        return result;
+    };
+
+    PokedexModel.prototype.changePokemon = function (direction) {
+        tts.speak(""); // stop speaking
         
+        this.imageNumber = this.getImageNumber(this.imageNumber, direction);
+        this.prevImageNumber = this.getImageNumber(this.imageNumber, -1);
+        this.nextImageNumber = this.getImageNumber(this.imageNumber, 1);
+
+        this.update();
+    };
+
+    PokedexModel.prototype.update = function () {
+        // current
         this.set("imageSource", "~/images/" + this.pokedex[this.imageNumber].fileName);
         this.set("name", this.pokedex[this.imageNumber].name);
         this.set("description", this.pokedex[this.imageNumber].description);
+        this.set("id", "#" + this.pad(this.imageNumber + 1, 3));
+
+        // prev
+        this.set("prevId", "#" + this.pad(this.prevImageNumber + 1, 3));
+        this.set("prevName", this.pokedex[this.prevImageNumber].name);
+
+        // next
+        this.set("nextId", "#" + this.pad(this.nextImageNumber + 1, 3));    
+        this.set("nextName", this.pokedex[this.nextImageNumber].name);
     };
 
     PokedexModel.prototype.onTap = function (args) {
-        if (args.object.id === "prev") {
+        if (args.object.id.includes("prev")) {
             this.changePokemon(-1);
         } else {
             this.changePokemon(1);
         }
+    };
+
+    PokedexModel.prototype.onSayName = function (args) {
+        tts.speak(this.pokedex[this.imageNumber].name, false, 1.75, 0.5, null);
     };
 
     PokedexModel.prototype.readDescription = function (args) {
