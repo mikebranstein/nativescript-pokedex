3967445359b2e592f08a738f9de80b651c116ec5
Mike Branstein <mike.branstein@gmail.com>
2016-08-23 20:43:36 -0500

    add font-icon plugin

--- app/app.js
+++ app/app.js
@@ -1,4 +1,12 @@
+var application = require("application");
+var TNSFontIcon = require("nativescript-fonticon").TNSFontIcon;
+var fonticon = require("nativescript-fonticon").fonticon;
 require("nativescript-platform");
 
-var application = require("application");
+TNSFontIcon.paths = {
+  "fa": "font-awesome.css"
+};
+TNSFontIcon.loadCss();
+
+application.resources["fonticon"] = fonticon;
 application.start({ moduleName: "main-page" });
